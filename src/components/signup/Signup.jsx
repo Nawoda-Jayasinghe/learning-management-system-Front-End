ab9b386d69b42cce6b6b28229969f41f34308a53
Nawoda-Jayasinghe <nawoda.jayasinghe98@gmail>
2022-10-14 15:23:22 +0530

    Sign up

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,37 +1,57 @@
-import {
-    Box,
-    Button,
-    FormHelperText,
-    IconButton,
-    Input,
-    InputAdornment,
-    MenuItem,
-    Select,
-    Stack,
-    TextField,
-    Typography,
-  } from "@mui/material";
-  import React, { useState } from "react";
-  import Appbar from "../home/Appbar";
-  // import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-  // import { Visibility, VisibilityOff } from "@mui/icons-material";
-  import Alert from "../utilsComponents/Alert";
-  import { Link, useNavigate } from "react-router-dom";
-//   import authService from "../../services/auth.service";
-  // import { ROLES } from "../../constants/roles";
-  import Footer from "../home/Footer";
+import React, { useState } from "react";
+import profile from "../../image/profile.png";
+import email from "../../image/email.jpg";
+import pass from "../../image/pass.png";
+import Typography from '@mui/material/Typography';
+import { Link, useNavigate } from "react-router-dom";
+import Box from '@mui/material/Box';
 
-  function Signup() {
-    return (
-        <>
-          <Appbar />
-          Signup
-          <Footer />
-        </>
-      );
-  
-  }
+function Signup() {
+  return (
+    <>
+      <Box >
+      <div className="sub-main">
+        <div>
+          <div className="imgs">
+            <div className="container-image">
+              <img src={profile} alt="profile" className="profile" />
+            </div>
+          </div>
+
+          <div>
+            <h1>Signup Page</h1>
+            <div>
+              <img src={email} alt="name" className="email" />
+              <input type="text" placeholder="user name" className="name" />
+            </div>
+            <br></br>
+            <div>
+              <img src={email} alt="email" className="email" />
+              <input type="text" placeholder="user email" className="name" />
+            </div>
+            <div className="second-input">
+              <img src={pass} alt="pass" className="pass" />
+              <input type="text" placeholder="password" className="name" />
+            </div>
+            
+            <div className="login-button">
+              <button>Signup</button>
+            </div>
+          </div>
+
+          <p className="link">
+            <a href="#"></a> Already have an account? <a href="#">Login</a>
+            
+             
+            
+          </p>
+          
+        </div>
+      </div>
+      </Box>
     
-  
-  export default Signup;
-  
+    </>
+  );
+}
+
+export default Signup;
