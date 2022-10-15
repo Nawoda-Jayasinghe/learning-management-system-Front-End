86ede94b4f6320a6b24649b8aff9e88a4450d350
Nawoda-Jayasinghe <nawoda.jayasinghe98@gmail>
2022-10-16 01:03:25 +0530

    Enrolled courses

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import Sidebar from "./Sidebar";
 import Content from "./Content";
 const Dashboard = () => {
-  return <div>
-    <Sidebar/>
-    {/* <Content/> */}
-  </div>;
+  return (
+    <div style={{ display: "flex" }}>
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+      </div>
+      <div style={{ display: "flex" }}>
+        <Content/>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
