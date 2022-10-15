9aff8d6df4128834417762d8e07f8cc86b9af66f
Nawoda-Jayasinghe <nawoda.jayasinghe98@gmail>
2022-10-15 16:14:39 +0530

    Subject card updated

--- src/components/student/StudentAvailable.jsx
+++ src/components/student/StudentAvailable.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Form from "antd/es/form/Form";
 import Grid from "antd/lib/card/Grid";
 import { Button } from "antd/lib/radio";
+import SubjectCard from "../dashboard/SubjectCard"
 
 function StudentAvailable() {
   return (
@@ -17,49 +18,20 @@ function StudentAvailable() {
             marginTop: "10px",
           }}
         >
-          <Grid container spacing={1} className="flex ">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkDNFIq-3knHkuUSzXtaujLmD9rzFkmNS02w&usqp=CAU"></img>
-            <Grid item lg={6} md={4} sm={6} xs={12} alignItems="center">
-              <center>
-                <label>MGTE 21212 Course</label>
-                <Button type="primary" block className="w-full">
-                  Enroll
-                </Button>
-              </center>
-            </Grid>
-          </Grid>
+        <SubjectCard/>
+        <SubjectCard/>
+        <SubjectCard/>
+        <SubjectCard/>
+          
 
-          <Grid container spacing={1} className="flex ">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkDNFIq-3knHkuUSzXtaujLmD9rzFkmNS02w&usqp=CAU"></img>
-            <Grid item lg={6} md={4} sm={6} xs={12}>
-              <label>MGTE 21213 Course</label>
-              <Button type="primary" block className="w-full">
-                Enroll
-              </Button>
-            </Grid>
-          </Grid>
-          <Grid container spacing={1} className="flex ">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkDNFIq-3knHkuUSzXtaujLmD9rzFkmNS02w&usqp=CAU"></img>
-            <Grid item lg={6} md={4} sm={6} xs={12}>
-              <label>MGTE 21214 Course</label>
-              <Button type="primary" block className="w-full">
-                Enroll
-              </Button>
-            </Grid>
-          </Grid>
-          <Grid container spacing={1} className="flex ">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkDNFIq-3knHkuUSzXtaujLmD9rzFkmNS02w&usqp=CAU"></img>
-            <Grid item lg={6} md={4} sm={6} xs={12}>
-              <label>MGTE 21215 Course</label>
-              <Button type="primary" block className="w-full">
-                Enroll
-              </Button>
-            </Grid>
-          </Grid>
+          
+          
+          
         </div>
       </Form>
     </div>
   );
 }
+
 
 export default StudentAvailable;
